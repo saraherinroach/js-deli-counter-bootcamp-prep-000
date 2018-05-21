5f7c495bdbf4ddd3783f73ce04af1de942b49cc4
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-21 14:58:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function currentLine (line) {
   if (line.length === 0) {
     return "The line is currently empty."
   } else { 
-  var countdown = line.length;
+  var countdown = line.length+1;
   var number = 1;
   while (countdown > 0) {
     var message = [`The line is currently: ${number}. ${line[number-1]}`];
